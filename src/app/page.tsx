beb3c97ddc27cbc3794eeb74198a1b621980d0be
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-03 20:27:17 -0300

    styles and new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,100 +4,45 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.bannerMain}>
+      <section className={styles.sectionBannerMain}>
         <div className={styles.containerTextBannerMain}>
           <h2>Desenvolvedor Fullstack e Arquiteto de Soluções</h2>
-          <p className={styles.animTypewriter}>Crio e Desenvolvo as Soluções</p>
+          <h3 className={styles.animTypewriter}>Crio e Desenvolvo as Soluções</h3>
+          <p>Proin scelerisque massa vel odio elementum, ac vestibulum nisl vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec a varius risus. Curabitur suscipit, quam vel pretium</p>
         </div>
 
         <img src='https://github.com/victor-magaldi.png'/>
       </section>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      
+ 
+      <hr/>
+      <section className={styles.sectionDescriptionMe}>
+        <h2>Sobre mim</h2>
+        <div className={styles.textMe}>
+          <p>Proin scelerisque massa vel odio elementum, ac vestibulum nisl vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec a varius risus. Curabitur suscipit, quam vel pretium ullamcorper, quam urna bibendum tellus, ac tincidunt nisi leo vel diam. Mauris sit amet ullamcorper elit. Sed interdum pulvinar urna, et venenatis enim volutpat sit amet. Quisque congue ex lacus, vel vulputate ligula egestas eu.Morbi porta, augue eu gravida hendrerit, massa augue tempor felis, id posuere felis nibh ac nibh. Phasellus pharetra vulputate elit, euismod malesuada orci viverra ut. Etiam tempor finibus sapien, nec pharetra odio porttitor ac. Fusce accumsan orci quis augue pellentesque, id volutpat ante feugiat. In blandit et eros sed volutpat. Nunc tellus quam, commodo et scelerisque a, semper et elit. Quisque accumsan vitae ipsum ac tincidunt. Maecenas in cursus magna. Donec eget luctus odio.</p>
+          <p>Morbi porta, augue eu gravida hendrerit, massa augue tempor felis, id posuere felis nibh ac nibh. Phasellus pharetra vulputate elit, euismod malesuada orci viverra ut. Etiam tempor finibus sapien, nec pharetra odio porttitor ac. Fusce accumsan orci quis augue pellentesque, id volutpat ante feugiat. In blandit et eros sed volutpat. Nunc tellus quam, commodo et scelerisque a, semper et elit. Quisque accumsan vitae ipsum ac tincidunt. Maecenas in cursus magna. Donec eget luctus odio.</p>
         </div>
-      </div>
+      </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <hr/>
+      <section className={styles.sectionSkills }>
+        <h2>Habilidades</h2>
+        <div className={styles.textMe}>
+          <p>Proin scelerisque massa vel odio elementum, ac vestibulum nisl vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec a varius risus. Curabitur suscipit, quam vel pretium ullamcorper, quam urna bibendum tellus, ac tincidunt nisi leo vel diam. Mauris sit amet ullamcorper elit. Sed interdum pulvinar urna, et venenatis enim volutpat sit amet. Quisque congue ex lacus, vel vulputate ligula egestas eu.Morbi porta, augue eu gravida hendrerit, massa augue tempor felis, id posuere felis nibh ac nibh. Phasellus pharetra vulputate elit, euismod malesuada orci viverra ut. Etiam tempor finibus sapien, nec pharetra odio porttitor ac. Fusce accumsan orci quis augue pellentesque, id volutpat ante feugiat. In blandit et eros sed volutpat. Nunc tellus quam, commodo et scelerisque a, semper et elit. Quisque accumsan vitae ipsum ac tincidunt. Maecenas in cursus magna. Donec eget luctus odio.</p>
+          <p>Morbi porta, augue eu gravida hendrerit, massa augue tempor felis, id posuere felis nibh ac nibh. Phasellus pharetra vulputate elit, euismod malesuada orci viverra ut. Etiam tempor finibus sapien, nec pharetra odio porttitor ac. Fusce accumsan orci quis augue pellentesque, id volutpat ante feugiat. In blandit et eros sed volutpat. Nunc tellus quam, commodo et scelerisque a, semper et elit. Quisque accumsan vitae ipsum ac tincidunt. Maecenas in cursus magna. Donec eget luctus odio.</p>
+        </div>
+      </section>
+      <hr/>
+      <section className={styles.projects }>
+        <h2>Projetos</h2>
+        
+      </section>
+    <hr/>
+      <section className={styles.contacts }>
+        <h2>Contatos</h2>
+        
+      </section>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
