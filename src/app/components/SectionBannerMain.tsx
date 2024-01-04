6eedc0b6680a288ef2a3cb6658b64ea32dc66352
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-04 19:26:40 -0300

    create component

---
+++ src/app/components/SectionBannerMain.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export function SectionBannerMain() {
+  return (
+        <section className={styles.sectionBannerMain}>
+        <div className={styles.containerTextBannerMain}>
+          <h2>Desenvolvedor Fullstack e Arquiteto de Soluções</h2>
+          <h3 className={styles.animTypewriter}>Crio e Desenvolvo as Soluções</h3>
+          <p>Proin scelerisque massa vel odio elementum, ac vestibulum nisl vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec a varius risus. Curabitur suscipit, quam vel pretium</p>
+        </div>
+
+        <img src='https://github.com/victor-magaldi.png' alt='victor-magaldi'/>
+      </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { SectionBannerMain } from './components/SectionBannerMain'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.sectionBannerMain}>
-        <div className={styles.containerTextBannerMain}>
-          <h2>Desenvolvedor Fullstack e Arquiteto de Soluções</h2>
-          <h3 className={styles.animTypewriter}>Crio e Desenvolvo as Soluções</h3>
-          <p>Proin scelerisque massa vel odio elementum, ac vestibulum nisl vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec a varius risus. Curabitur suscipit, quam vel pretium</p>
-        </div>
-
-        <img src='https://github.com/victor-magaldi.png'/>
-      </section>
-      
+ 
+        <SectionBannerMain/>
  
       <hr/>
       <section className={styles.sectionDescriptionMe}>
