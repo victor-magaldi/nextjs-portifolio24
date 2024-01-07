e8977a8267a66e68168c52b165092ca0c70880c7
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-07 18:19:58 -0300

    structure description

--- src/components/SectionDescription/index.tsx
+++ src/components/SectionDescription/index.tsx
@@ -6,34 +6,40 @@ export function SectionDescription() {
     <section className={styles.sectionDescriptionMe}>
       <div className="container">
         <h2>Sobre mim</h2>
-        <div className={styles.textMe}>
-          <p>
-            Proin scelerisque massa vel odio elementum, ac vestibulum nisl
-            vehicula. Vestibulum eleifend mollis nulla, vel semper nisl.
-            Curabitur vitae neque pretium, facilisis velit in, semper leo. Donec
-            a varius risus. Curabitur suscipit, quam vel pretium ullamcorper,
-            quam urna bibendum tellus, ac tincidunt nisi leo vel diam. Mauris
-            sit amet ullamcorper elit. Sed interdum pulvinar urna, et venenatis
-            enim volutpat sit amet. Quisque congue ex lacus, vel vulputate
-            ligula egestas eu.Morbi porta, augue eu gravida hendrerit, massa
-            augue tempor felis, id posuere felis nibh ac nibh. Phasellus
-            pharetra vulputate elit, euismod malesuada orci viverra ut. Etiam
-            tempor finibus sapien, nec pharetra odio porttitor ac. Fusce
-            accumsan orci quis augue pellentesque, id volutpat ante feugiat. In
-            blandit et eros sed volutpat. Nunc tellus quam, commodo et
-            scelerisque a, semper et elit. Quisque accumsan vitae ipsum ac
-            tincidunt. Maecenas in cursus magna. Donec eget luctus odio.
-          </p>
-          <p>
-            Morbi porta, augue eu gravida hendrerit, massa augue tempor felis,
-            id posuere felis nibh ac nibh. Phasellus pharetra vulputate elit,
-            euismod malesuada orci viverra ut. Etiam tempor finibus sapien, nec
-            pharetra odio porttitor ac. Fusce accumsan orci quis augue
-            pellentesque, id volutpat ante feugiat. In blandit et eros sed
-            volutpat. Nunc tellus quam, commodo et scelerisque a, semper et
-            elit. Quisque accumsan vitae ipsum ac tincidunt. Maecenas in cursus
-            magna. Donec eget luctus odio.
-          </p>
+        <p>
+          Proin scelerisque massa vel odio elementum, ac vestibulum nisl
+          vehicula. Vestibulum eleifend mollis nulla, vel semper nisl. Curabitur
+          vitae neque pretium, facilisis velit in, semper leo
+        </p>
+        <div className={styles.textMeContainer}>
+          <img
+            src="https://github.com/victor-magaldi.png"
+            alt="victor-magaldi-avatar-image"
+            width={500}
+            height={500}
+            className={styles.imageProfile}
+          />
+          <div className={styles.textMe}>
+            <h3>Desenvolvedor Fullstack e Arquiteto de Soluções</h3>
+            <p>
+              Proin scelerisque massa vel odio elementum, ac vestibulum nisl
+              vehicula. Vestibulum eleifend mollis nulla, vel semper nisl.
+              Curabitur vitae neque pretium, facilisis velit in, semper leo.
+            </p>
+            <ul>
+              <li>Nascimento: 17/03/1994</li>
+              <li>Idade: 29</li>
+              <li>Cidade: Nova Friburgo, RJ, Brasil</li>
+              <li>E-mail: victmagaldi@hotmail.com</li>
+              <li>Nível: Especialista</li>
+              <li>Freelancer: disponível</li>
+            </ul>
+            <p>
+              Proin scelerisque massa vel odio elementum, ac vestibulum nisl
+              vehicula. Vestibulum eleifend mollis nulla, vel semper nisl.
+              Curabitur vitae neque pretium, facilisis velit in, semper leo.
+            </p>
+          </div>
         </div>
       </div>
     </section>
