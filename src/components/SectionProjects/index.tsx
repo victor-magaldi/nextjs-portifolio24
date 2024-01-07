f75d551610f0545245cba52fe20816cb061fca33
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-07 17:37:25 -0300

    projects sessioon init

--- src/components/SectionProjects/index.tsx
+++ src/components/SectionProjects/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import styles from './styles.css' 
+import React from "react";
+import styles from "./styles.module.css";
 
 export function SectionProjects() {
   return (
-    <section className={styles.projects }>
+    <section className={styles.projects}>
       <div className="container">
-        <h2>Projetos</h2>   
+        <h2>Projetos</h2>
       </div>
-    
     </section>
-  )
+  );
 }
