6179f0e8d6288056e21a9b358884f4a837216cc0
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-08 19:49:15 -0300

    effect window in card project

--- src/components/SectionProjects/index.tsx
+++ src/components/SectionProjects/index.tsx
@@ -10,79 +10,22 @@ export function SectionProjects() {
           Projetos desenvolvidos ao longo da minha jornada de estudo e trabalho
         </p>
         <div className={styles.projectLists}>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
+          {Array.from({ length: 12 }, (_, index) => index).map((item) => {
+            return (
+              <div className={styles.projectCard}>
+                <div className={styles.containerProjectCardImg}>
+                  <img
+                    src="https://placehold.co/250?text=Projeto1"
+                    className={styles.projectCardImg}
+                    alt="Projeto1"
+                  />
+                </div>
 
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
-          <div className={styles.projectCard}>
-            <img
-              src="https://placehold.co/250?text=Projeto1"
-              className={styles.projectCardImg}
-              alt="Projeto1"
-            />
-            <h4 className={styles.projectName}>Projeto 1 </h4>
-            <button className="">Ver mais</button>
-          </div>
+                <h4 className={styles.projectName}>Projeto 1 </h4>
+                <button className="">Ver mais</button>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
