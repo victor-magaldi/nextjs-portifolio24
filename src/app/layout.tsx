c976a9f590cd4fba7046f55991fa73881b9b9448
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-09 20:06:01 -0300

    Insert container modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <div id="modal-root"></div>
       </body>
     </html>
   );
