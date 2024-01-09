3078a2b7b429d60983eabadf7b9da342de2979e5
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-09 19:48:16 -0300

    Modal logic searchParam

--- src/components/SectionProjects/index.tsx
+++ src/components/SectionProjects/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+"use client";
+
 import styles from "./styles.module.css";
+import { useSearchParams } from "next/navigation";
 
 export function SectionProjects() {
+  const searchParams = useSearchParams();
+  const modalOpen = searchParams.get("modalOpen");
+
   return (
     <section className={styles.projects}>
       <div className="container">
@@ -12,7 +17,12 @@ export function SectionProjects() {
         <div className={styles.projectLists}>
           {Array.from({ length: 12 }, (_, index) => index).map((item) => {
             return (
-              <div className={styles.projectCard} key={String(item)}>
+              <div
+                className={styles.projectCard}
+                key={String(item)}
+                id={String(item)}
+              >
+                {modalOpen === String(item) && <span>Open</span>}
                 <div className={styles.containerProjectCardImg}>
                   <img
                     src="https://placehold.co/250?text=Projeto1"
