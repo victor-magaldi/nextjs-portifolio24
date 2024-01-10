ea8c5dd2077f34b97f450e920c4c5ef3730d3d41
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-10 20:06:40 -0300

    Init Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.css";
+
+export function Footer() {
+  return (
+    <footer>
+      <h4>Victor Magaldi</h4>
+      <p>Minhas Redes: </p>
+    </footer>
+  );
+}
