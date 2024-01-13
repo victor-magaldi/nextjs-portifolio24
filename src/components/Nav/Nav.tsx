c098ccfcc45b2b34dea018b404f651aefa524b38
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-13 15:22:35 -0300

    initi animation menu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,22 +3,25 @@ import styles from "./styles.module.css";
 export function Navigation() {
   return (
     <nav className={styles.navigationContainer}>
-      <ul className={styles.listLinks}>
-        <li className={styles.linkItem}>
-          <a href="/#description" className={styles.link}>
-            Sobre Mim
-          </a>
-        </li>
-        <li className={styles.link}>
-          <a href="/#projects">Projetos</a>
-        </li>
-        <li className={styles.link}>
-          <a href="/#skills">Habilidades</a>
-        </li>
-        <li className={styles.link}>
-          <a href="/#contact">Fale comigo</a>
-        </li>
-      </ul>
+      <div className={styles.containerList}>
+        <ul className={styles.listLinks}>
+          <li className={styles.linkItem}>
+            <a href="/#description" className={styles.link}>
+              ICONE
+              <span>Sobre Mim</span>
+            </a>
+          </li>
+          <li className={styles.link}>
+            <a href="/#projects">Projetos</a>
+          </li>
+          <li className={styles.link}>
+            <a href="/#skills">Habilidades</a>
+          </li>
+          <li className={styles.link}>
+            <a href="/#contact">Fale comigo</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
