0ef9356fc0cac685fc75e1cef56ad09bbbf92ee5
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-13 14:51:17 -0300

    Create Nav

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,20 @@
+export function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/#summary">Sobre Mim</a>
+        </li>
+        <li>
+          <a href="/#projects">Projetos</a>
+        </li>
+        <li>
+          <a href="/#skills">Habilidades</a>
+        </li>
+        <li>
+          <a href="/#contact">Fale comigo</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
